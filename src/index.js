b9ee05063fd82047b12027405b1734946ef48219
thars <paraneios@gmail.com>
2020-07-04 11:19:30 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var app1 =  (
+  <div>
+    <App name="max"/>
+    <App name="max2"/>
+    </div>
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    app1,
   document.getElementById('root')
 );
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
